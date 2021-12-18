4cbd9f9ff2e039c7e63b6ebf90834a1fea47a4e9
lightfindyou <3148438700@qq.com>
2021-12-18 22:22:07 +0800

    try to compile .so

--- src/destor.c
+++ src/destor.c
@@ -4,11 +4,44 @@
  *  Created on: May 24, 2012
  *      Author: fumin
  */
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "destor.h"
 #include "jcr.h"
 #include "index/index.h"
 #include "storage/containerstore.h"
+
+/* Function pointers to hold the value of the glibc functions */
+static  ssize_t (*real_write)(int fd, const void *buf, size_t count) = NULL;
+static int (*real_puts)(const char* str) = NULL;
+
+/* wrapping write function call */
+ssize_t write(int fd, const void *buf, size_t count) {
+
+    /* printing out the number of characters */
+    printf("write:chars#:%lu\n", count);
+    /* reslove the real write function from glibc
+     * and pass the arguments.
+     */
+    real_write = dlsym(RTLD_NEXT, "write");
+    real_write(fd, buf, count);
+
+}
+
+int puts(const char* str) {
+
+    /* printing out the number of characters */
+    printf("puts:chars#:%lu\n", strlen(str));
+    /* resolve the real puts function from glibc
+     * and pass the arguments.
+     */
+    real_puts = dlsym(RTLD_NEXT, "puts");
+    real_puts(str);
+}
+
 
 struct destor destor;
 extern void do_backup(char *path);
