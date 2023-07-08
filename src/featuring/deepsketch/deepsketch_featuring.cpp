4443f56e166478cdcbf0bcc49c7c50a59a9d7bae
lightfindyou <3148438700@qq.com>
2023-07-08 11:10:15 -0400

    able to compile with libtorch

--- src/featuring/deepsketch/deepsketch_featuring.cpp
+++ src/featuring/deepsketch/deepsketch_featuring.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include "deepsketch_featuring.h"
 #include <torch/script.h>
+#include <iostream>
+
+#include "deepsketch_featuring.h"
 #include "../../destor.h"
 
-extern "C"
-void deepsketch_featuring_init(char* modelPath){
+typedef std::bitset<HASH_SIZE> MYHASH;
 
-}
+extern "C" void deepsketch_featuring_init(char *modelPath) {}
 
-extern "C"
-void deepsketch_featuring(unsigned char* buf, int size, struct chunk* c) {
+extern "C" void deepsketch_featuring(unsigned char *buf, int size,
+                                     struct chunk *c) {
     std::cout << "calling c++ functions " << std::endl;
 }
 
@@ -17,18 +17,18 @@ class NetworkHash {
    private:
     int BATCH_SIZE;
     torch::jit::script::Module module;
-    float* data;
-    bool* memout;
-    int* index;
+    float *data;
+    bool *memout;
+    int *index;
     int cnt;
 
    public:
-    NetworkHash(int BATCH_SIZE, char* module_name) {
+    NetworkHash(int BATCH_SIZE, char *module_name) {
         this->BATCH_SIZE = BATCH_SIZE;
         this->module = torch::jit::load(module_name);
         this->module.to(at::kCUDA);
         this->module.eval();
-        this->data = new float[BATCH_SIZE * BLOCK_SIZE *2];
+        this->data = new float[BATCH_SIZE * BLOCK_SIZE * 2];
         this->memout = new bool[BATCH_SIZE * HASH_SIZE];
         this->index = new int[BATCH_SIZE];
         this->cnt = 0;
@@ -38,16 +38,16 @@ class NetworkHash {
         delete[] this->memout;
         delete[] this->index;
     }
-    bool push(char* ptr, int size, int label);
+    bool push(char *ptr, int size, int label);
     std::vector<std::pair<MYHASH, int>> request();
 };
 
-bool NetworkHash::push(char* ptr, int size, int label) {
-    if(cnt == 0){
-        memset(this->data, 0, sizeof(float)*BATCH_SIZE * BLOCK_SIZE * 2);
+bool NetworkHash::push(char *ptr, int size, int label) {
+    if (cnt == 0) {
+        memset(this->data, 0, sizeof(float) * BATCH_SIZE * BLOCK_SIZE * 2);
     }
     for (int i = 0; i < size; ++i) {
-        data[cnt * BLOCK_SIZE *2 + i] =
+        data[cnt * BLOCK_SIZE * 2 + i] =
             ((int)(unsigned char)(ptr[i]) - 128) / 128.0;
     }
     index[cnt++] = label;
@@ -66,7 +66,7 @@ std::vector<std::pair<MYHASH, int>> NetworkHash::request() {
 
     std::vector<torch::jit::IValue> inputs;
     torch::Tensor t =
-        torch::from_blob(data, {cnt, BLOCK_SIZE*2}).to(torch::kCUDA);
+        torch::from_blob(data, {cnt, BLOCK_SIZE * 2}).to(torch::kCUDA);
     inputs.push_back(t);
 
     // it seems here calculates the hash value
@@ -76,7 +76,7 @@ std::vector<std::pair<MYHASH, int>> NetworkHash::request() {
     torch::Tensor comp = output.ge(0.0);
     memcpy(memout, comp.cpu().data_ptr<bool>(), cnt * HASH_SIZE);
 
-    bool* ptr = this->memout;
+    bool *ptr = this->memout;
 
     for (int i = 0; i < cnt; ++i) {
         for (int j = 0; j < HASH_SIZE; ++j) {
--- src/featuring/deepsketch/deepsketch_featuring.h
+++ src/featuring/deepsketch/deepsketch_featuring.h
@@ -1,3 +1,9 @@
+#ifndef __DEEPSKETCH_FEATURING_
+#define __DEEPSKETCH_FEATURING_
+
+#define HASH_SIZE 128
+#define BLOCK_SIZE 4096
+
 #ifdef __cplusplus
 extern "C"
 #endif
@@ -7,3 +13,8 @@ void deepsketch_featuring(unsigned char* buf, int size, struct chunk* c);
 extern "C"
 #endif
 void deepsketch_featuring_init(char* modelPath);
+
+
+
+
+#endif  //__DEEPSKETCH_FEATURING_
